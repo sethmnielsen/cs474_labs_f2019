26dda95844e6c744cc1f05fa247d7bb63c851f3d
Seth Nielsen <sethmnielsen@gmail.com>
2019-10-11 18:57:53 -0600

    Auto-pushed changes

--- lab5/lab5.py
+++ lab5/lab5.py
@@ -1,46 +1,46 @@
 # To add a new cell, type '#%%'
 # To add a new markdown cell, type '#%% [markdown]'
 #%% [markdown]
-# <a href="https://colab.research.google.com/github/sethmnielsen/cs474_labs_f2019/blob/master/DL_Lab5.ipynb" target="_parent"><img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/></a>
+#  <a href="https://colab.research.google.com/github/sethmnielsen/cs474_labs_f2019/blob/master/DL_Lab5.ipynb" target="_parent"><img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/></a>
 #%% [markdown]
-# # Lab 5: Style Transfer
+#  # Lab 5: Style Transfer
 # 
-# ## Objective
-# To explore an alternative use of DNNs by implementing the style transfer algorithm.
-# To understand the importance of a complex loss function.
-# To see how we can optimize not only over network parameters,
-# but over other objects (such as images) as well.
+#  ## Objective
+#  To explore an alternative use of DNNs by implementing the style transfer algorithm.
+#  To understand the importance of a complex loss function.
+#  To see how we can optimize not only over network parameters,
+#  but over other objects (such as images) as well.
 # 
-# ## Deliverable
-# For this lab, you will need to implement the style transfer algorithm of Gatys et al.
+#  ## Deliverable
+#  For this lab, you will need to implement the style transfer algorithm of Gatys et al.
 # 
-# * You must extract statistics from the content and style images
-# * You must formulate an optimization problem over an input image
-# * You must optimize the image to match both style and content
+#  * You must extract statistics from the content and style images
+#  * You must formulate an optimization problem over an input image
+#  * You must optimize the image to match both style and content
 # 
-# In your jupyter notebook, you should turn in the following:
+#  In your jupyter notebook, you should turn in the following:
 # 
-# * The final image that you generated
-# * Your code
+#  * The final image that you generated
+#  * Your code
 # 
-# An example image that I generated is shown below
+#  An example image that I generated is shown below
 # 
-# ![](http://liftothers.org/dokuwiki/lib/exe/fetch.php?w=300&tok=179805&media=cs501r_f2016:style1.png)
+#  ![](http://liftothers.org/dokuwiki/lib/exe/fetch.php?w=300&tok=179805&media=cs501r_f2016:style1.png)
 # 
-# ## Grading standards
-# Your code will be graded on the following:
+#  ## Grading standards
+#  Your code will be graded on the following:
 # 
-# * 35% Correct extraction of statistics
-# * 45% Correct construction of loss function in a loss class
-# * 10% Correct initialization and optimization of image variable in a dataset class
-# * 10% Awesome looking final image
+#  * 35% Correct extraction of statistics
+#  * 45% Correct construction of loss function in a loss class
+#  * 10% Correct initialization and optimization of image variable in a dataset class
+#  * 10% Awesome looking final image
 # 
-# ## Description:
+#  ## Description:
 # 
-# For this lab, you should implement the style transfer algorithm referenced above.
-# To do this, you will need to unpack the given images.
-# Since we want you to focus on implementing the paper and the loss function, 
-# we will give you the code for this.
+#  For this lab, you should implement the style transfer algorithm referenced above.
+#  To do this, you will need to unpack the given images.
+#  Since we want you to focus on implementing the paper and the loss function,
+#  we will give you the code for this.
 
 #%%
 import torch
@@ -67,6 +67,7 @@ __ITB__ = AutoFormattedTB(mode = 'Verbose', color_scheme='LightBg', tb_offset = 
 from google.colab import files
 
 
+
 #%%
 
 load_and_normalize = transforms.Compose([
@@ -75,20 +76,22 @@ load_and_normalize = transforms.Compose([
     transforms.ToTensor(),
 ])
 
-def upload():
-  print('Upload Content Image')
-  file_dict = files.upload()
-  content_path = io.BytesIO(file_dict[next(iter(file_dict))])
+# def upload():
+#   print('Upload Content Image')
+#   file_dict = files.upload()
+#   content_path = io.BytesIO(file_dict[next(iter(file_dict))])
 
-  print('\nUpload Style Image')
-  file_dict = files.upload()
-  style_path = io.BytesIO(file_dict[next(iter(file_dict))])
-  return content_path, style_path
+#   print('\nUpload Style Image')
+#   file_dict = files.upload()
+#   style_path = io.BytesIO(file_dict[next(iter(file_dict))])
+#   return content_path, style_path
 
-content_path, style_path = upload()
+# content_path, style_path = upload()
+content_path = "/home/seth/Pictures/style_transfer/iss043e93251_lrg.jpg"
+style_path = "/home/seth/Pictures/style_transfer/modernism.jpg"
+print(f"Content Path: {content_path}")
+print(f"Style Path: {style_path}")
 
-print("Content Path: {}".format(content_path))
-print("Style Path: {}".format(style_path))
 
 
 #%%
@@ -99,18 +102,18 @@ style_image_orig = Image.open(style_path)
 style_image = load_and_normalize(np.array(style_image_orig)).unsqueeze(0).cuda()
 
 #%% [markdown]
-# ___
+#  ___
 # 
-# ### Part 1
-# Create a class to extract the layers needed for statistics
+#  ### Part 1
+#  Create a class to extract the layers needed for statistics
 # 
-# **TODO:**
+#  **TODO:**
 # 
-# * Use the pretrained VGG in your model
-# * Gather statistics from the outputs of intermediate layers for the content image
-# * Gather statistics for the style image
+#  * Use the pretrained VGG in your model
+#  * Gather statistics from the outputs of intermediate layers for the content image
+#  * Gather statistics for the style image
 # 
-# **DONE:**
+#  **DONE:**
 # 
 # 
 
@@ -148,6 +151,7 @@ class VGGIntermediate(nn.Module):
     return self.intermediates
 
 
+
 #%%
 vgg_names = ["conv1_1", "relu1_1", "conv1_2", "relu1_2", "maxpool1", "conv2_1", "relu2_1", "conv2_2", "relu2_2", "maxpool2", "conv3_1", "relu3_1", "conv3_2", "relu3_2", "conv3_3", "relu3_3","maxpool3", "conv4_1", "relu4_1", "conv4_2", "relu4_2", "conv4_3", "relu4_3","maxpool4", "conv5_1", "relu5_1", "conv5_2", "relu5_2", "conv5_3", "relu5_3","maxpool5"]
 
@@ -165,20 +169,17 @@ style_layers = style_vgg(style_img)
 content_layers = content_vgg(content_img)
 
 
-#%%
-vgg_names = ["conv1_1", "relu1_1", "conv1_2", "relu1_2", "maxpool1", "conv2_1", "relu2_1", "conv2_2", "relu2_2", "maxpool2", "conv3_1", "relu3_1", "conv3_2", "relu3_2", "conv3_3", "relu3_3","maxpool3", "conv4_1", "relu4_1", "conv4_2", "relu4_2", "conv4_3", "relu4_3","maxpool4", "conv5_1", "relu5_1", "conv5_2", "relu5_2", "conv5_3", "relu5_3","maxpool5"]
-content_layers = 
 
 #%% [markdown]
 # ___
 # 
-# ### Part 2
-# Create a method to turn a tensor to an image to display
+#  ### Part 2
+#  Create a method to turn a tensor to an image to display
 # 
-# **TODO:**
-# * Display the style tensor and content tensor transformed back to an image
+#  **TODO:**
+#  * Display the style tensor and content tensor transformed back to an image
 # 
-# **DONE:**
+#  **DONE:**
 # 
 
 #%%
@@ -201,19 +202,20 @@ display(content_image, title='Content Image')
 #%% [markdown]
 # ___
 # 
-# ### Part 3
-# Create a classes for the style and content loss
+#  ### Part 3
+#  Create a classes for the style and content loss
 # 
-# **TODO:**
+#  **TODO:**
 # 
-# * Create a module that calculates the content loss in the forward method, compared to some precalculated targets stored in the class
-# * Create a module that calculates the style loss in the forward method using a gram matrix, compared to some precalculated targets stored in the class
+#  * Create a module that calculates the content loss in the forward method, compared to some precalculated targets stored in the class
+#  * Create a module that calculates the style loss in the forward method using a gram matrix, compared to some precalculated targets stored in the class
 # 
-# **DONE:**
+#  **DONE:**
 # 
 
 #%%
 def gram_matrix(input):
+
    pass
   
 class ContentLoss(nn.Module):
@@ -229,21 +231,21 @@ class StyleLoss(nn.Module):
 #  with the style reference image outputs for that layer for comparison
 
 #%% [markdown]
-# ___
+#  ___
 # 
-# ### Part 4
-# Create and run a method that minimizes the content and style loss for a copy of the content image
+#  ### Part 4
+#  Create and run a method that minimizes the content and style loss for a copy of the content image
 # 
-# Note that the content loss should be zero if you take out the style loss. Why is that?
+#  Note that the content loss should be zero if you take out the style loss. Why is that?
 # 
-# **TODO:**
+#  **TODO:**
 # 
-# * Use an Adam optimizer with learning rate of .1
-# * Show both the content and the style loss every 50 steps
-# * Ensure that the outputs don't go out of range (clamp them)
-# * Display the tensor as an image!
+#  * Use an Adam optimizer with learning rate of .1
+#  * Show both the content and the style loss every 50 steps
+#  * Ensure that the outputs don't go out of range (clamp them)
+#  * Display the tensor as an image!
 # 
-# **DONE:**
+#  **DONE:**
 # 
 # 
 
@@ -255,5 +257,6 @@ class StyleLoss(nn.Module):
 #  to backpropagate errors 
   
 # Show the image
+
 
 
