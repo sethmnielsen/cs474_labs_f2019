e63872eb657a14ae3512099059f9ef630e54235e
Seth Nielsen <sethmnielsen@gmail.com>
2019-10-23 13:14:26 -0600

    Auto-pushed changes

--- text_files/parse_sw_script.py
+++ text_files/parse_sw_script.py
@@ -1,23 +1,23 @@
 with open('combined_dialogues.txt', 'r') as f:
-    with open('starwars_data.txt', 'w') as new_f:
+    with open('starwars_data2.txt', 'w') as new_f:
         for line in f:
             if line.isupper():
                 continue
             else:
-                if line[0] == '(' and line[-2] == ')':
-                    continue
-                else:
-                    s = ''
-                    for word in line.split():
-                        if word.isupper() or word[0] == '(' or word[-1] == ')':
+                s = ''
+                for word in line.split():
+                    if word.isupper() or word[0] == '#':
+                        if word != 'I':
                             continue
-                        if word.startswith('Artoo-Detoo'):
-                            word = 'R2-D2'
-                        elif word.startswith('Artoo'):
-                            word = 'R2'
-                        elif word.startswith('See-Threepio'):
-                            word = 'C-3PO'
-                        elif word[4:12] == 'Threepio':
-                            word = '3PO'
-                        s += word + ' '
-                    new_f.write(s)
+                    if word.startswith('Artoo-Detoo'):
+                        word = 'R2-D2' + word[11:]
+                    elif word.startswith('Artoo'):
+                        word = 'R2' + word[5:]
+                    elif word.startswith('See-Threepio'):
+                        word = 'C-3PO' + word[12:]
+                    elif word.startswith('Threepio'):
+                        word = '3PO' + word[8:]
+                    elif word == '(cont)':
+                        continue
+                    s += word + ' '
+                new_f.write(s)
